5364c4a76cdc350b7013212cb38745554135fed7
marllusvinicius04 <103164792+marllusvinicius04@users.noreply.github.com>
2026-07-27 09:56:50 -0300

    Update script.js

--- catalogos/script.js
+++ catalogos/script.js
@@ -1,5 +1,5 @@
 
-const API_URL="https://script.google.com/macros/s/AKfycbxOjQ_sUd5f5_yUCl-YCJNyI0HqaxsB1DCBMDtaeIzrd-Qf_aZR9qkAkQ9Pyzgd_Dd6/exec";
+const API_URL="https://script.google.com/macros/s/AKfycbyn3065wcnSaDbtTGkjf78a-E5xvuyTn_grtEbWaS3LO8ziPX_I8BmrCKb3NzE3Mk_Y/exec";
 const COMPANY_ID=""; // TROQUE APENAS ESTE CÓDIGO PELO ID DA EMPRESA
 const AUTO_REFRESH_MS=5000;
 const $=id=>document.getElementById(id);
